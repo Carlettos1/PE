01b3aa5204227a21475df137cc7a226737aedc01
Carlettos <kako3000@gmail.com>
2021-12-06 00:15:48 -0300

    refractor and working on 2

--- Project Euler/src/com/carlettos/core/PERun.java
+++ Project Euler/src/com/carlettos/core/PERun.java
@@ -5,13 +5,13 @@ package com.carlettos.core;
  * @author Carlos
  */
 public abstract class PERun {
-    
-    public PERun(){
+
+    public PERun() {
     }
-    
+
     protected abstract void run();
-    
-    public static void run(PERun problem){
+
+    public static void run(PERun problem) {
         problem.run();
     }
 }
--- Project Euler/src/com/carlettos/five/Problem1.java
+++ Project Euler/src/com/carlettos/five/Problem1.java
@@ -3,11 +3,15 @@ package com.carlettos.five;
 import com.carlettos.core.PERun;
 
 /**
- *  <p>If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.</p>
-    <p>Find the sum of all the multiples of 3 or 5 below 1000.</p>
+ * <p>
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+ * get 3, 5, 6 and 9. The sum of these multiples is 23.</p>
+ * <p>
+ * Find the sum of all the multiples of 3 or 5 below 1000.</p>
+ *
  * @author Carlos
  */
-public class Problem1 extends PERun{
+public class Problem1 extends PERun {
 
     @Override
     protected void run() {
