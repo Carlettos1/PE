3b0552d29731e6868cc60aa28fd8db15b49fe529
Carlettos <kako3000@gmail.com>
2021-12-06 00:09:40 -0300

    Added core

---
+++ Project Euler/src/com/carlettos/core/PERun.java
@@ -0,0 +1,17 @@
+package com.carlettos.core;
+
+/**
+ *
+ * @author Carlos
+ */
+public abstract class PERun {
+    
+    public PERun(){
+    }
+    
+    protected abstract void run();
+    
+    public static void run(PERun problem){
+        problem.run();
+    }
+}
